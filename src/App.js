2f85a60fa788c2ff59b9d37949170b350eefa177
Pratham thakur <hustlerpratham@gmail.com>
2024-12-28 22:58:26 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Hello</h1>;
+  return <h1>Hello|React</h1>;
+}
+
+function Pratham() {
+  return <h2>Pratham Thakur</h2>;
 }
 
 export default App;
+export { Pratham };
